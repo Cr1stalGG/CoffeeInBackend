de2b2f754b820d05b617adf01d95425b86685cf1
= <=>
2024-09-19 16:14:32 +0300

    add AccountController

---
+++ src/main/java/com/example/controller/AccountController.kt
@@ -0,0 +1,32 @@
+package com.example.controller
+
+import com.example.dto.account.AccountFullDto
+import com.example.dto.account.AccountShortcutDto
+import com.example.service.AccountService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
+
+@RestController
+@RequestMapping("/api/v1/accounts")
+class AccountController (
+    val accountService: AccountService
+){
+    @GetMapping
+    fun findAll(): List<AccountShortcutDto>? {
+        return accountService.findAll()
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable("id") id: UUID): AccountFullDto {
+        return accountService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable("id") id: UUID){
+        return accountService.deleteById(id)
+    }
+}
