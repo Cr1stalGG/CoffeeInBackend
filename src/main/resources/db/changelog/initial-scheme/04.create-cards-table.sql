4ba708b244b54c84ae798e5306b95c3da6b58323
= <=>
2024-09-26 14:26:23 +0300

    Add money to card

--- src/main/resources/db/changelog/initial-scheme/04.create-cards-table.sql
+++ src/main/resources/db/changelog/initial-scheme/04.create-cards-table.sql
@@ -3,5 +3,6 @@ create table cards(
     number varchar(16) unique not null,
     password varchar(64) unique not null,
     cvv varchar(64) unique not null,
+    money numeric not null check(money >= 0),
     account_id uuid references accounts(id)
 );
--- src/main/resources/db/db-init.sql
+++ src/main/resources/db/db-init.sql
@@ -23,6 +23,7 @@ create table cards(
     number varchar(16) unique not null,
     password varchar(64) unique not null,
     cvv varchar(64) unique not null,
+    money numeric not null check(money > 0),
     account_id uuid references accounts(id)
 );
 
